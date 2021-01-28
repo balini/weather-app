2afb9b7c6766cf054a5370af1e0e56bb1d81181d
balini <mariliacmg@gmail.com>
2021-01-27 22:23:55 -0300

    consumindo open weather api com express.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const result = require('dotenv').config();
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+if (result.error) {
+    throw result.error;
+}
+
+const apiKey = process.env.API_KEY;
+console.log(result);
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+});
+
+app.post('/', function (req, res) {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    request(url, function (err, response, body) {
+        if(err){
+            res.render('index', {weather: null, error: 'Error processing, please try again.'});
+        } else {
+            let weather = JSON.parse(body);
+            if(weather.main == undefined || weather.main.feels_like == undefined){
+                res.render('index', {weather: null, error: 'Error, please type a valid city.'});
+            } else {
+                let weatherText = `It's ${weather.main.temp} celsius degrees in ${weather.name}! But it feels like it's ${weather.main.feels_like} celsius.`;
+                res.render('index', {weather: weatherText, error: null});
+            }
+        } 
+    });
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
